2cd80cd1c7ccf864aa798905eca38c7369851265
lta2705 <le13572630@gmail.com>
2025-07-03 12:49:33 +0700

    added redis

--- src/main/java/com/example/live/config/RedisConfiguration.java
+++ src/main/java/com/example/live/config/RedisConfiguration.java
@@ -4,7 +4,6 @@ import com.example.live.service.redis.SubscriberService;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.event.EventListener;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
--- src/main/java/com/example/live/service/redis/TcpHandler.java
+++ src/main/java/com/example/live/service/redis/TcpHandler.java
@@ -1,10 +1,9 @@
-package com.example.live.service;
+package com.example.live.service.redis;
 
 import jakarta.annotation.PostConstruct;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Service;
 
 import java.io.IOException;
